8a4f4530b2c5e08f02c34cd67a3370b837ef8c43
Cyber <elricleung@126.com>
2021-03-21 21:34:01 +0800

    Add files via upload

---
+++ .github/workflows/bing.py
@@ -0,0 +1,30 @@
+# # coding = utf8
+
+import requests
+import json
+from os.path import exists
+from os import stat
+from time import strftime, localtime
+
+
+# 获取bing每日壁纸,下载图片模式
+def get_the_wallpaper():
+    fileName = '../../img/bing.jpg'
+    if not exists(fileName) or strftime("%Y%m%d", localtime(stat(fileName).st_mtime)) != strftime("%Y%m%d", localtime()):
+        bingURL = 'https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN'
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) Chrome/52.0.2743.116 Edge/15.15063'
+        }
+        try:
+            wallJson = requests.get(bingURL, headers).text
+            wallURL = json.loads(wallJson)['images'][0]['url']
+            wallFullURL = 'http://cn.bing.com' + wallURL
+            response = requests.get(wallFullURL)
+            with open(fileName, 'wb') as f:
+                f.write(response.content)
+        except:
+            pass
+
+
+if __name__=='__main':
+    get_the_wallpaper()
