ab0d2bd476322b99ad8fa83b94a1b85496114604
Cyber <elricleung@126.com>
2021-03-30 21:50:06 +0800

    Update bing.py

--- .github/workflows/bing.py
+++ .github/workflows/bing.py
@@ -2,28 +2,21 @@
 
 import requests
 import json
-from os.path import exists
-from os import stat
-from time import strftime, localtime
 
 
 # 获取bing每日壁纸,下载图片模式
 def get_the_wallpaper():
-    fileName = '../../img/bing.jpg'
-    if not exists(fileName) or strftime("%Y%m%d", localtime(stat(fileName).st_mtime)) != strftime("%Y%m%d", localtime()):
-        bingURL = 'https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN'
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) Chrome/52.0.2743.116 Edge/15.15063'
-        }
-        try:
-            wallJson = requests.get(bingURL, headers).text
-            wallURL = json.loads(wallJson)['images'][0]['url']
-            wallFullURL = 'http://cn.bing.com' + wallURL
-            response = requests.get(wallFullURL)
-            with open(fileName, 'wb') as f:
-                f.write(response.content)
-        except:
-            pass
+    fileName = 'bing.jpg'
+    bingURL = 'https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN'
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) Chrome/52.0.2743.116 Edge/15.15063'
+    }
+    wallJson = requests.get(bingURL, headers).text
+    wallURL = json.loads(wallJson)['images'][0]['url']
+    wallFullURL = 'http://cn.bing.com' + wallURL
+    response = requests.get(wallFullURL)
+    with open(fileName, 'wb') as f:
+         f.write(response.content)
 
 
 if __name__ == '__main__':
