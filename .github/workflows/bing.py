ec089a83a70ed7876a3c7d6816acd6d1c265dff1
Cyber <elricleung@126.com>
2021-03-30 22:02:19 +0800

    Update bing.py

--- .github/workflows/bing.py
+++ .github/workflows/bing.py
@@ -7,7 +7,7 @@ import json
 fileName = 'bing.jpg'
 bingURL = 'https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN'
 headers = {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) Chrome/52.0.2743.116 Edge/15.15063'
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) Chrome/52.0.2743.116 '
 }
 wallJson = requests.get(bingURL, headers).text
 wallURL = json.loads(wallJson)['images'][0]['url']
